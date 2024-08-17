bd8e1ffaa1bbd05cbc3787a86d0afbdb2c7a59f1
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-17 21:46:49 +0600

    Project File Created with Next.js Initial Commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Counter from "@/components/Counter/Counter";
+import Link from "next/link";
+
+const HomePage = () => {
+  return (
+    <div className="">
+      <h1>This is the Home Page</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default HomePage;
