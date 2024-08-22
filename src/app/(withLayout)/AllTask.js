52c6802dc1bbb684050c957ace4a4abc4ccb03f3
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-22 14:01:56 +0600

    Task Style Updated

--- src/app/(withLayout)/AllTask.js
+++ src/app/(withLayout)/AllTask.js
@@ -7,6 +7,7 @@ import Swal from "sweetalert2";
 import { motion } from "framer-motion";
 import useAuth from "@/Hooks/Auth/useAuth";
 import useAxiosSecure from "@/Hooks/Axios/useAxiosSecure";
+import Loading from "../loading";
 
 const AllTask = () => {
   const axios = useAxiosSecure();
@@ -25,6 +26,18 @@ const AllTask = () => {
   const [openPop, setOpenPop] = useState(false);
   const [updateInfo, setUpdateInfo] = useState({});
 
+  // Get Task
+  async function getTasks() {
+    try {
+      const res = await axios.get(`/tasks/${uId}`);
+      console.log("all task by uid:", res.data);
+      setTasks(res.data);
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+    }
+  }
+
+  // Get Boards
   async function getBoards() {
     try {
       const res = await axios.get(`/categories/${uId}`);
@@ -38,24 +51,15 @@ const AllTask = () => {
     }
   }
 
-  async function getTasks() {
-    try {
-      const res = await axios.get(`/tasks/${uId}`);
-      console.log("all task by uid:", res.data);
-      setTasks(res.data);
-    } catch (error) {
-      console.error("Error fetching tasks:", error);
-    }
-  }
-
-  useEffect(() => {
-    getTasks();
-  }, [updateTaskList]);
+  // useEffect(() => {
+  //   getTasks();
+  // }, [updateTaskList]);
 
   useEffect(() => {
     getBoards();
   }, []);
 
+  // On Drag Update Card Category/Board
   const onDragEnd = async (result) => {
     const { destination, source, draggableId } = result;
 
@@ -64,7 +68,6 @@ const AllTask = () => {
     if (!destination) {
       return;
     }
-
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
@@ -90,6 +93,7 @@ const AllTask = () => {
     }
   };
 
+  // Handle Delete Single Task
   async function handleDelete(id) {
     console.log(id);
     const res = await axios.delete(`/tasks/${id}`);
@@ -104,13 +108,18 @@ const AllTask = () => {
     }
   }
 
+  // Handle Update Task
   function handleUpdate(task) {
     setUpdateInfo(task);
     setOpenPop(true);
   }
 
   if (loading) {
-    return <>Loading</>;
+    return (
+      <>
+        <Loading />
+      </>
+    );
   }
 
   return (
@@ -125,20 +134,23 @@ const AllTask = () => {
             hidden: { opacity: 0, x: -50 },
             visible: { opacity: 1, x: 0 },
           }}
-          className="container flex flex-col gap-10 p-4 mx-auto todo-list md:flex-row"
+          className="container flex flex-col gap-8 p-4 mx-auto todo-list md:flex-row flex-wrap w-full justify-center"
         >
           {boardList?.map((list) => {
             return (
               <Droppable droppableId={list?.boardname} key={list.id}>
                 {(provided) => (
                   <div
-                    className="p-4 text-white bg-blue-700 rounded-sm category md:w-1/3"
+                    className="p-4 text-white bg-[#1f1f1f] rounded-sm category md:w-1/4"
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                   >
-                    <h2 className="mb-5 text-4xl font-bold text-yellow-400">
-                      {list.boardname}
-                    </h2>
+                    <div className="flex justify-between mb-5 md:text-3xl font-bold text-white">
+                      <p>{list.boardname}</p>
+                      <button className=" text-red-500">
+                        <MdDeleteForever />
+                      </button>
+                    </div>
                     {tasks
                       .filter((task) => task.category === list.boardname)
                       .map((task, index) => (
@@ -149,35 +161,47 @@ const AllTask = () => {
                         >
                           {(provided) => (
                             <div
-                              className="mb-4 task"
+                              className="mb-4 bg-gray-950 p-2 rounded-lg"
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
                               ref={provided.innerRef}
                             >
-                              <div className="flex flex-row items-center justify-between gap-2">
-                                <div className="flex-1">
-                                  <p>Task Title</p>
-                                  <p>{task.title}</p>
+                              <div className="flex justify-between">
+                                {/* Title And DadeLine */}
+                                <div className=" space-y-2">
+                                  <div className="">
+                                    <p className="italic">Task Title:</p>
+                                    <p className="text-xl font-medium">
+                                      {task.title}
+                                    </p>
+                                  </div>
+                                  <div className="flex gap-2">
+                                    <p className="font-medium italic">
+                                      Deadline:
+                                    </p>
+                                    <p>{task.deadline}</p>
+                                  </div>
                                 </div>
-                                <div className="flex-1">
-                                  <p>Due date</p>
-                                  <p>{task.deadline}</p>
+                                {/* Action Button  */}
+                                <div className="flex flex-col items-center gap-2">
+                                  <button
+                                    onClick={() => handleUpdate(task)}
+                                    className="h-auto p-2 ml-2 min-h-min bg-[#313131] hover:bg-[#383838] rounded-lg "
+                                  >
+                                    <FaRegEdit />
+                                  </button>
+                                  <button
+                                    className="h-auto p-2 ml-2 min-h-min bg-[#313131] hover:bg-[#383838] hover:text-red-500 rounded-lg "
+                                    onClick={() => handleDelete(task.id)}
+                                  >
+                                    <MdDeleteForever />
+                                  </button>
                                 </div>
-                                <button
-                                  onClick={() => handleUpdate(task)}
-                                  className="h-auto p-2 ml-2 btn min-h-min"
-                                >
-                                  <FaRegEdit />
-                                </button>
-                                <button
-                                  className="h-auto p-2 btn min-h-min"
-                                  onClick={() => handleDelete(task.id)}
-                                >
-                                  <MdDeleteForever />
-                                </button>
                               </div>
-                              <div>
-                                <p>Task Description</p>
+
+                              {/* Description */}
+                              <div className="mt-2 ">
+                                <p className="italic">Task Description:</p>
                                 <p>{task.description}</p>
                               </div>
                             </div>
@@ -192,6 +216,8 @@ const AllTask = () => {
           })}
         </motion.div>
       </DragDropContext>
+
+      {/* Modal For Update Task */}
       <div
         className={`h-screen flex flex-col items-center justify-center fixed inset-0 bg-black bg-opacity-90 ${
           openPop ? "block" : "hidden"
