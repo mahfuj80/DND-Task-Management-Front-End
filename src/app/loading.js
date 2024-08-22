3b99cbedd33aedc4fca69c01ed13f35797311f15
mahfuj80 <mahfujurrahman06627@gmail.com>
2024-08-22 14:19:18 +0600

    Loading and not found page modified

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,8 +2,19 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div>
-      <h1 className="text-4xl">Loading...</h1>
+    <div className="h-screen justify-center items-center">
+      <div className="text-6xl  text-center font-semibold flex gap-2 items-center mt-12 md:mt-24">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-10 animate-spin fill-white block mx-auto"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z"
+            data-original="#000000"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,22 +3,23 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <section className="bg-white dark:bg-gray-900 h-screen flex flex-col items-center justify-center">
+    <section className="bg-slate-900 h-screen flex flex-col items-center justify-center">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-white ">
             404
           </h1>
-          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          <p className="mb-4 text-3xl tracking-tight font-bold text-white md:text-4xl ">
             Something&apos;s missing.
           </p>
-          <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+          <p className="mb-4 text-lg font-light text-gray-500 ">
             Sorry, we can&apos;t find that page. You&apos;ll find lots to
             explore on the home page.
           </p>
           <Link
             href="/"
-            className="inline-flex text-white bg-black  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+            className="inline-flex text-white bg-black  focus:ring-4 focus:outline-none
+             rounded-lg text-sm px-5 py-2.5 text-center font-bold my-4"
           >
             Back to Homepage
           </Link>
